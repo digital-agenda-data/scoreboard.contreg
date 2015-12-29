d0bf605ed863b1a802aaa4a419d5550500176e34
heinlja <jaanus.heinlaid@gmail.com>
2015-12-29 13:55:41 +0200

    Preparations for Liquibase usage in CR.

---
+++ sql/virtuoso-database-preparation.sql
@@ -0,0 +1,40 @@
+-- ---------------------------------------------------------------------
+-- Create DB users.
+-- ---------------------------------------------------------------------
+
+USER_CREATE('cr3user', 'xxx', vector ('SQL_ENABLE',1,'DAV_ENABLE',1));
+USER_CREATE('cr3rouser', 'yyy', vector ('DAV_ENABLE',1));
+USER_CREATE('cr3test', 'zzz', vector ('SQL_ENABLE',1,'DAV_ENABLE',1));
+
+-- ---------------------------------------------------------------------
+-- Grant permissions to DB users.
+-- ---------------------------------------------------------------------
+
+USER_GRANT_ROLE('cr3user','SPARQL_SELECT',0);
+USER_GRANT_ROLE('cr3user','SPARQL_UPDATE',0);
+GRANT SELECT ON sys_rdf_schema TO cr3user;
+GRANT execute ON rdfs_rule_set TO cr3user;
+
+USER_GRANT_ROLE('cr3test','SPARQL_SELECT',0);
+USER_GRANT_ROLE('cr3test','SPARQL_UPDATE',0);
+GRANT SELECT ON sys_rdf_schema TO cr3test;
+GRANT execute ON rdfs_rule_set TO cr3test;
+
+USER_GRANT_ROLE('cr3rouser','SPARQL_SELECT',0);
+
+-- ---------------------------------------------------------------------------
+-- Set DB users' default databases.
+-- ---------------------------------------------------------------------------
+
+user_set_qualifier ('cr3user', 'CR');
+user_set_qualifier ('cr3rouser', 'CR');
+user_set_qualifier ('cr3test', 'CRTEST');
+
+-- ---------------------------------------------------------------------------
+-- Set up full-text indexing in the triplestore.
+-- ---------------------------------------------------------------------------
+
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, null, 'All');
+DB.DBA.VT_INC_INDEX_DB_DBA_RDF_OBJ ();
+DB.DBA.VT_BATCH_UPDATE ('DB.DBA.RDF_OBJ', 'OFF', null);
+DB.DBA.VT_BATCH_UPDATE ('DB.DBA.RDF_OBJ', 'ON', 10);
---
+++ src/main/java/eionet/cr/liquibase/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Classes related to the usage of Liquibase database refactoring API.
+ */
+package eionet.cr.liquibase;
+
